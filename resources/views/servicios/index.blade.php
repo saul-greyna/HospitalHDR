98dc46122240318aae74b73955f91f7afad3b3c7
saul-greyna <saulreynag.07@gmail.com>
2026-07-07 13:06:31 -0600

    Produccion Julio Final

--- resources/views/servicios/index.blade.php
+++ resources/views/servicios/index.blade.php
@@ -1,3 +1,2 @@
 <x-main title="Servicios · Hospital Dr. Raúl Hernández"
-         description="Servicios médicos y promociones del Hospital Dr. Raúl Hernández" />
-    
+    description="Servicios médicos y promociones del Hospital Dr. Raúl Hernández" />
